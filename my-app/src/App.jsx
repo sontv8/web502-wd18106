f36a0b10c3321ec269544fc1caecab804f43a924
sontv8 <sontv8@fpt.edu.vn>
2023-07-17 08:56:01 +0700

    day5 - add product

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,32 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pages/Home'
 
+const products = [
+  { id: 1, name: 'product 1', price: 100 },
+  { id: 2, name: 'product 2', price: 200 },
+  { id: 3, name: 'product 3', price: 300 },
+]
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HomePage data={products} />
     </>
   )
 }
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Header, Footer } from './components'
 
-const menus = [
-  { id: 1, name: "Home", path: '/' },
-  { id: 2, name: "About", path: '/about' },
-  { id: 3, name: "Product", path: '/product' },
-]
-// props
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
-    <Header menus={menus} />
-    <main></main>
-    <Footer />
-  </>
+  <App />
 )
